0dafc010407f8335847e2bdb95488e488ef7daff
michaelga <xanmichael@gmail.com>
2018-03-07 01:31:49 +0200

    parse json

--- app/src/main/java/com/example/royok/finalproject/MainActivity.kt
+++ app/src/main/java/com/example/royok/finalproject/MainActivity.kt
@@ -5,6 +5,7 @@ import kotlinx.android.synthetic.main.activity_main.*
 import android.app.Activity
 import android.app.ProgressDialog.show
 import android.os.Bundle
+import com.beust.klaxon.JsonArray
 import com.beust.klaxon.JsonObject
 import com.beust.klaxon.Parser
 import kotlinx.android.synthetic.main.activity_main.*
@@ -23,15 +24,32 @@ class MainActivity : Activity() {
 
         startBtn.setOnClickListener()
         {
+            var candleTime:String =""
+            var parasha:String=""
+            var havdala:String =""
             val dialog = show(this, "",
                     "find location", true)
             async {
                 val result = URL("\t\n" + "http://www.hebcal.com/shabbat/?cfg=json&geo=pos&latitude=31.771959&longitude=35.217018&tzid=Asia/Jerusalem&m=50&b=35").readText()
-                val parser = Parser()
                 val stringBuilder = StringBuilder(result)
+                val parser = Parser()
                 val json: JsonObject = parser.parse(stringBuilder) as JsonObject
-                var itemsJson  = json.getValue("items")
-
+                val items : JsonArray<JsonObject> = json.get("items") as JsonArray <JsonObject>
+                for (i in 0..items.size-1)
+                {
+                    if(items[i].containsValue("candles"))
+                    {
+                        candleTime = items[i].getValue("title") as String
+                    }
+                    else if (items[i].containsValue("parashat"))
+                    {
+                        parasha = items[i].getValue("hebrew") as String
+                    }
+                    else if (items[i].containsValue("havdalah"))
+                    {
+                        havdala = items[i].getValue("title") as String
+                    }
+                }
 
                 //var candleTimes:JsonObject = itemsJson[0] as JsonObject
 
@@ -39,13 +57,14 @@ class MainActivity : Activity() {
                 textView.post(
                         {
                             dialog.cancel()
-                            textView.text = itemsJson.toString()
+                            textView.text = parasha+"\n"+candleTime+"\n"+havdala
                         })
             }
 
             /*val i = Intent(this,Main2Activity::class.java)           //go to the next activity
             startActivity(i)*/
         }
+
 
         /*val stringArray = ArrayList<String>()
         val jsonArray = JSONArray()
@@ -62,5 +81,6 @@ class MainActivity : Activity() {
             i++
         }*/
     }
+
 
 }
