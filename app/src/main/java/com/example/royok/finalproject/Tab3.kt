85d0551df54381f7abdd6d016b3c5de8eb61921f
michaelga <xanmichael@gmail.com>
2018-03-11 09:14:11 +0200

    add view elements

--- app/src/main/java/com/example/royok/finalproject/Tab3.kt
+++ app/src/main/java/com/example/royok/finalproject/Tab3.kt
@@ -5,15 +5,20 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
 import kotlinx.android.synthetic.main.tab_1.view.*
 
 /**
  * Created by royok on 06/03/2018.
  */
 class Tab3 : Fragment(){
+//    var mTimePicker : timePicker ?= null
+    var saveTime : Button?= null
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
                               savedInstanceState: Bundle?): View? {
         val rootView = inflater.inflate(R.layout.tab_3, container, false)
+        saveTime = rootView.findViewById(R.id.setTime)
+        saveTime?.setOnClickListener() {  }
         return rootView
     }
 }
